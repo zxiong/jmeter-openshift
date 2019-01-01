8efeff415479b9f40137beed98a07a40b6626655
root <root@bugzilla-qe-01.host.stage.eng.rdu2.redhat.com>
2019-01-01 13:40:50 +0000

    add comments for the script

--- openshift/run_test.sh
+++ openshift/run_test.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+#Script aims to
+# 1) create JMeter clusters
+# 2) run performance test and copy test results back 
+# 3) TODO: remove JMeter clusters 
+
 if [ $# -lt 3 ]; then
     echo "Please input parameters are not enough, please check them"
     exit 1
@@ -9,9 +14,7 @@ WORKSPACE=$1
 BUILD_NUMBER=$2
 TEST_SCRIPT=$3
 
-#source ~/.bashrc
-oc login https://paas.upshift.redhat.com --token=00T3lcRsUq906MUNeRY1nsDuMf4eBxbGXUZtqS5uXLo
-
+# create JMeter clusters
 cd $WORKSPACE/openshift && sh jmeter_cluster_create.sh
 
 # create workspace
@@ -22,5 +25,8 @@ cd $WORKSPACE/openshift
 test_script_path="$WORKSPACE/$TEST_SCRIPT"
 test_script_dir="$(dirname $test_script_path)"
 
+# run test and copy test results back to Jenkins slave.
 sh start_test.sh "$TEST_SCRIPT" "$test_script_dir"
 cp /tmp/test_result.jtl $WORKSPACE/$PERFCI_WORKING_DIR/
+
+# TODO: remove JMeter clusters
